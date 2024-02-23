4895e7b131973d78d45b64dc26eb517d43fac572
srrscx <cxyreussadaya@gmail.com>
2024-02-23 09:20:56 +0800

    home typebar & navigation responsiveness

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -6,17 +6,18 @@ import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import batstateulogo from "../../assets/images/batStateUNeu-logo.png";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
-  const [shadow, setShadow] = useState(true);
-  const [navBg, setNavBg] = useState("black");
-  const [linkColor, setLinkColor] = useState("white");
+  function toggleNavbar() {
+    const sidebar = document.querySelector(".tab-nav");
+    if (sidebar.style.display === "none") {
+      sidebar.style.display = "flex";
+    } else {
+      sidebar.style.display = "none";
+    }
+  }
 
   return (
     <section>
-      <div className="loading-overlay" id="loadingOverlay">
-        <div className="loading-spinner" />
-      </div>
-      <div className="navbar-container">
+      <div className="shadow navbar-container">
         <div className="left">
           <div className="left-md-container">
             <div className="left-sm-container">
@@ -51,43 +52,35 @@ const Navbar = () => {
                 Sign In
               </a>
             </ul>
-            <div className="device">
-              <div className="container">
-                <button id="burger" className="open-main-nav">
-                  <span className="burger" />
-                  <span className="burger-text">Menu</span>
-                </button>
-                <nav className="main-nav" id="main-nav">
-                  <ul>
-                    <li>
-                      <a href="#">About me</a>
-                    </li>
-                    <li>
-                      <a href="#">Speaker Writer</a>
-                    </li>
-                    <li>
-                      <a href="#">Work</a>
-                    </li>
-                    <li>
-                      <a href="#">Contact</a>
-                    </li>
-                    <li>
-                      <a href="#">Blog</a>
-                    </li>
-                  </ul>
-                </nav>
-                <p className="notice">
-                  Design Copyright <strong>Stéphanie Walter</strong>
-                  <br />
-                  <a href="https://stephaniewalter.design/" target="_blank">
-                    StephanieWalter.Design
-                  </a>
-                </p>
-              </div>
+            <div className="burger" onClick={toggleNavbar}>
+              <AiOutlineMenu size={30} />
+            </div>
+            <div className="close" onClick={toggleNavbar}>
+              <AiOutlineClose size={30} />
             </div>
           </div>
         </div>
+
       </div>
+        <div className="tab-nav">
+          <ul>
+            <li className="home-link">
+              <Link href="/">HOME</Link>
+            </li>
+            <li className="ip-assets-link">
+              <Link href="/ip-assets">IP ASSETS</Link>
+            </li>
+            <li className="pb-link">
+              <Link href="/abt-link">PUBLICATIONS</Link>
+            </li>
+            <li className="abt-link">
+              <Link href="/about">ABOUT US</Link>
+            </li>
+            <a href="/" className="signin-btn">
+              Sign In
+            </a>
+          </ul>
+        </div>
     </section>
   );
 };
